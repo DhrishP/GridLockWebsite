7894031ab5a3c3f62a2f2a8394d81b688b088c87
Dhrish Parekh <parekhdhrish.pg@gmail.com>
2023-10-13 20:15:26 +0530

    added the basic logic and some UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
-
+"use client";
+import LoginForm from "@/components/loginform";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    // Perform localStorage action
+    const verify = async () => {
+      //@ts-ignore
+      const username =await JSON.parse(localStorage.getItem("username"));
+      //@ts-ignore
+      const secret =await JSON.parse(localStorage.getItem("secret"));
+      if (username && secret) {
+       return null
+      } else {
+        localStorage.setItem("username", JSON.stringify(""));
+        localStorage.setItem("secret", JSON.stringify(""));
+      }
+    };
+    verify()
+  }, []);
   return (
     <>
-      <div>hi</div>
+      <LoginForm />
     </>
   );
 }
--- app/statspage/page.tsx
+++ app/statspage/page.tsx
@@ -1,9 +1,62 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { redirect } from "next/navigation";
 
+type dataschema = {
+  id: string;
+  username: string;
+  red_score: number;
+  green_score: number;
+  date: string;
+};
 const StatsPage = () => {
-  return (
-    <div>StatsPage</div>
-  )
-}
+  const [data, setdata] = useState([] as dataschema[]);
+  useEffect(() => {
+    const getdata = async () => {
+      const username = localStorage.getItem("username");
+      const secret = localStorage.getItem("secret");
+      if (!username || !secret) window.location.href = "/";
+      const data = await axios.post("/api/scores", { username, secret });
+      if (!data) return (window.location.href = "/");
+      console.log(data);
+      setdata(data.data);
+    };
+    getdata();
+  }, []);
+  let wins: number;
+  let lose: number;
+  {
+    data.length > 0 &&
+      data.map((user) => {
+        if (user.green_score > user.red_score) {
+          wins++;
+        } else {
+          lose++;
+        }
+      });
+  }
 
-export default StatsPage
+  return (
+    <div>
+      {data.length > 0 ? (
+        data.map((user) => (
+          <div key={user.id}>
+            {user.username}
+            {user.red_score}
+            {user.green_score}
+          </div>
+        ))
+      ) : (
+        <>
+          <p>
+            Enter correct username/secret or create one by playing the{" "}
+            <a href="">game</a>
+          </p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default StatsPage;
