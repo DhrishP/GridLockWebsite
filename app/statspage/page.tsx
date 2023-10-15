4491b6194302c4117f8428a04b78d7e0c7ede35b
Dhrish Parekh <parekhdhrish.pg@gmail.com>
2023-10-16 03:10:35 +0530

    fixed alot of prerender errors

--- app/statspage/page.tsx
+++ app/statspage/page.tsx
@@ -2,10 +2,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
-
 import NotFoundPage from "@/components/errorpage";
 import Stats from "@/components/statspage";
-
+import { useRouter } from "next/navigation";
 type dataschema = {
   id: string;
   username: string;
@@ -14,15 +13,27 @@ type dataschema = {
   date: string;
 };
 const StatsPage = () => {
-  const username = localStorage.getItem("username");
-  const secret = localStorage.getItem("secret");
+  const router = useRouter();
+  const [username, setUsername] = useState("");
+  const [secret, setSecret] = useState("");
+
+  useEffect(() => {
+    const user = localStorage.getItem("username");
+    setUsername(user ? user : "");
+    const sec = localStorage.getItem("secret");
+    setSecret(sec ? sec : "");
+  }, []);
   let wins: number = 0;
-  let lose: number= 0;
-  let ratio:number = 0;
-  if (!username || !secret) window.location.href = "/";
-  const { data, isLoading, isError } = useQuery({
+  let lose: number = 0;
+  let ratio: number = 0;
+  if (typeof window !== "undefined" && (!username || !secret)) {
+    router.push("/statspage");
+  }
+
+  const { data, isLoading, isError, isSuccess } = useQuery({
     queryKey: ["stats"],
-    queryFn: async () => axios.post("/api/scores", { username, secret }),
+    queryFn: () => axios.post("/api/scores", { username, secret }),
+    enabled: !!username && !!secret,
   });
 
   if (isLoading)
@@ -31,37 +42,43 @@ const StatsPage = () => {
         <div className="w-5 h-5 rounded-full border-4 animate-spin border-b-white border-slate-400 "></div>
       </div>
     );
-  if (isError || data.data.length == 0 ) return <NotFoundPage/>;
-  {
-    data.data.length > 0 &&
-      data.data.map((user: dataschema) => {
-        if (user.green_score > user.red_score) {
-          wins++;
-        } else {
-          lose++;
-        }
-      });
-  }
-  ratio = lose === 0 ? wins : wins / lose;
-  console.log(wins, lose);
-  console.log(ratio);
-  const highestscore = Math.max(...data.data.map((user: dataschema) => user.green_score));
-  console.log(highestscore);
-  return (
-    <div>
-      {data.data.length > 0 ? (
-       <Stats highestscore={highestscore} ratio={ratio} wins={wins} />
-      ) : (
-        <>
-          <div>
-            <div className="flex items-center justify-center">
-              <div className="w-4 h-4 bg-slate-400 rounded-full animate-spin border-b-white border border-slate-400"></div>
+  if (isError || data.data.length == 0) return <NotFoundPage />;
+  if (isSuccess) {
+    {
+      data.data.length > 0 &&
+        data.data.map((user: dataschema) => {
+          if (user.green_score > user.red_score) {
+            wins++;
+          } else {
+            lose++;
+          }
+        });
+    }
+    ratio = lose === 0 ? wins : wins / lose;
+    const highestscore = Math.max(
+      ...data.data.map((user: dataschema) => user.green_score)
+    );
+    return (
+      <div>
+        {data.data.length > 0 ? (
+          <Stats
+            username={username ? username : ""}
+            highestscore={highestscore}
+            ratio={ratio}
+            wins={wins}
+          />
+        ) : (
+          <>
+            <div>
+              <div className="flex items-center justify-center">
+                <div className="w-4 h-4 bg-slate-400 rounded-full animate-spin border-b-white border border-slate-400"></div>
+              </div>
             </div>
-          </div>
-        </>
-      )}
-    </div>
-  );
+          </>
+        )}
+      </div>
+    );
+  }
 };
 
 export default StatsPage;
